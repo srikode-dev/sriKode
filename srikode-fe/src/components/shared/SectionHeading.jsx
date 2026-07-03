e2bf59dbf5c894259ed0e43c09ed4b5ef71cb5d6
Srikant114 <srikantsahu114@gmail.com>
2026-07-04 00:00:41 +0530

    chore: remove unused file and associated configuration references

--- srikode-fe/src/components/shared/SectionHeading.jsx
+++
@@ -1,9 +0,0 @@
-export default function SectionHeading({ title, subtitle }) {
-  return (
-    <div className="mb-12">
-      <h2 className="h2">{title}</h2>
-
-      {subtitle && <p className="mt-3 text-muted">{subtitle}</p>}
-    </div>
-  );
-}
